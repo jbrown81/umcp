ab0e956707d1e0706f45b5789d116cfd0cc7b2c0
Jesse Brown <jbrown81@gmail.com>
2018-03-07 13:52:22 -0800

    updated spatial corr with spearman correlation

--- core.py
+++ core.py
@@ -623,13 +623,13 @@ def spatial_corr(input_filename1,input_filename2,mask_filename=None,type='pearso
     
         mask_coords = np.nonzero(input3_d)
         if type is 'spearman':
-            input_corr = scipy.stats.spearmanr(input1_d[mask_coords],input2_d[mask_coords])
+            [input_corr, p] = scipy.stats.spearmanr(input1_d[mask_coords],input2_d[mask_coords])
         else:
             input_corr = np.corrcoef(input1_d[mask_coords],input2_d[mask_coords])
             input_corr = input_corr[0,1]
     else:
         if type is 'spearman':
-            input_corr = scipy.stats.spearmanr(input1_d.flatten(),input2_d.flatten())
+            [input_corr, p] = scipy.stats.spearmanr(input1_d.flatten(),input2_d.flatten())
         else:
             input_corr = np.corrcoef(input1_d.flatten(),input2_d.flatten())
             input_corr= input_corr[0,1]
